1f799e0e3c4136939e6b8acd33227829f002b125
valeryyakishkin <velyakishkin@gmail.com>
2023-01-17 18:00:01 +0300

    showed past moves

--- src/index.js
+++ src/index.js
@@ -88,6 +88,18 @@ class Game extends React.Component {
     const history = this.state.history;
     const current = history[history.length - 1];
     const winner = calculateWinner(current.squares);
+
+    const moves = history.map((_, move) => {
+      const desc = move 
+        ? 'Move to #' + move
+        : 'Move to start game';
+      return (
+        <li>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      )
+    })
+
     let status;
     if (winner) {
       status = winner + " won";
@@ -105,7 +117,7 @@ class Game extends React.Component {
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
